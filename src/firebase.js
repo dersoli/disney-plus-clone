004b02b23ee5179eb2b0a54a575d97be09e2c1f2
Anderson Xavier <dersoli@gmail.com>
2023-09-26 22:13:52 -0300

    setup firebase

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCfzyPwjO-z75Ez2C9ncQ0PYSBnlesEL40",
+    authDomain: "dersoli-disneyplus-clone.firebaseapp.com",
+    projectId: "dersoli-disneyplus-clone",
+    storageBucket: "dersoli-disneyplus-clone.appspot.com",
+    messagingSenderId: "721448162984",
+    appId: "1:721448162984:web:907bc7ec0a5fb976e7149d",
+    measurementId: "G-E85Y23S7XE"
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider =  new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
+
+export {auth, provider, storage};
+export default db;
