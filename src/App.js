f6647a361fdc0b627d7e0689ced22a86d0de61dd
Anderson Xavier <dersoli@gmail.com>
2023-09-27 20:36:24 -0300

    AuthContextProvider added to App.js (firebase)

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login';
 import Header from './components/Header';
+import { AuthContextProvider } from './context/AuthContext';
 
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route exact path='/' element={<Login/>} />
-        </Routes>
-      </Router>
+      <AuthContextProvider>
+        <Router>
+          <Header />
+          <Routes>
+            <Route exact path='/' element={<Login/>} />
+          </Routes>
+        </Router>
+      </AuthContextProvider>
       
     </div>
   );
